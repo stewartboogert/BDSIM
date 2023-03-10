45018e58c9267fee5a81c5ebcc0061dd0c42e53a
sboogert <stewart.boogert@rhul.ac.uk>
2023-03-10 15:27:26 +0000

    Remove artifact storage
    Former-commit-id: f4a1329713008201db33357b658ef8dafa99699e

--- python/fastlistPybind.cc
+++ python/fastlistPybind.cc
@@ -9,4 +9,11 @@ namespace py = pybind11;
 PYBIND11_MODULE(fastlist, m) {
   py::class_<GMAD::FastList<GMAD::Element>>(m,"FastList_Element")
     .def(py::init<>());
+
+  py::class_<GMAD::FastList<GMAD::Element>::FastListIterator>(m,"FastList_Element_Iterator");
+  py::class_<GMAD::FastList<GMAD::Element>::FastListConstIterator>(m,"FastList_Element_ConstIterator");
+  py::class_<GMAD::FastList<GMAD::Element>::FastMapIterator>(m,"FastMap_Element_Iterator");
+  py::class_<GMAD::FastList<GMAD::Element>::FastMapConstIterator>(m,"FastMap_Element_ConstIterator");
+  py::class_<GMAD::FastList<GMAD::Element>::FastMapIteratorPair>(m,"FastMap_Element_IteratorPair");
+  py::class_<GMAD::FastList<GMAD::Element>::FastMapConstIteratorPair>(m,"FastMap_Element_ConstIteratorPair");
 }
