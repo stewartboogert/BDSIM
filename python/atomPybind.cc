2b20f7c8ebcfe7ac271d7b9aa035ff82ecd662c5
sboogert <stewart.boogert@rhul.ac.uk>
2023-03-18 21:18:06 +0000

    Indent fix
    Former-commit-id: a69000ccffe21af93e0e5d9e3a5371164586b4d2

--- python/atomPybind.cc
+++ python/atomPybind.cc
@@ -13,17 +13,17 @@ PYBIND11_MODULE(atom, m) {
 py::class_<GMAD::Published<GMAD::Atom>>(m,"PublishedAtom")
 .def("NameExists",&GMAD::Atom::NameExists);
 
-py::class_<GMAD::Atom, GMAD::Published<GMAD::Atom>>(m,"Atom")
-.def(py::init<>())
-.def_readwrite("name", &GMAD::Atom::name)
-.def_readwrite("A", &GMAD::Atom::A)
-.def_readwrite("Z", &GMAD::Atom::Z)
-.def_readwrite("symbol", &GMAD::Atom::symbol)
-.def("clear",&GMAD::Atom::clear)
-.def("print",&GMAD::Atom::print)
-.def("set_value",[](GMAD::Atom &atom,std::string name,std::string value) {atom.set_value<std::string>(name,value);})
-.def("set_value",[](GMAD::Atom &atom,std::string name,int value) {atom.set_value<int>(name,value);})
-.def("set_value",[](GMAD::Atom &atom,std::string name,bool value) {atom.set_value<bool>(name,value);})
-.def("set_value",[](GMAD::Atom &atom,std::string name,long int value) {atom.set_value<long int>(name,value);})
-.def("set_value",[](GMAD::Atom &atom,std::string name,double value) {atom.set_value<double>(name,value);});
+  py::class_<GMAD::Atom, GMAD::Published<GMAD::Atom>>(m,"Atom")
+    .def(py::init<>())
+    .def_readwrite("name", &GMAD::Atom::name)
+    .def_readwrite("A", &GMAD::Atom::A)
+    .def_readwrite("Z", &GMAD::Atom::Z)
+    .def_readwrite("symbol", &GMAD::Atom::symbol)
+    .def("clear",&GMAD::Atom::clear)
+    .def("print",&GMAD::Atom::print)
+    .def("set_value",[](GMAD::Atom &atom,std::string name,std::string value) {atom.set_value<std::string>(name,value);})
+    .def("set_value",[](GMAD::Atom &atom,std::string name,int value) {atom.set_value<int>(name,value);})
+    .def("set_value",[](GMAD::Atom &atom,std::string name,bool value) {atom.set_value<bool>(name,value);})
+    .def("set_value",[](GMAD::Atom &atom,std::string name,long int value) {atom.set_value<long int>(name,value);})
+    .def("set_value",[](GMAD::Atom &atom,std::string name,double value) {atom.set_value<double>(name,value);});
 }
